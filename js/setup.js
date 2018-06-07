58bc749c3741b4c05641be274374cbc95885536d
Vika <elegant_17@list.ru>
2018-06-07 19:49:23 +0300

    Сгенерированы волшебники

---
+++ js/setup.js
@@ -0,0 +1,61 @@
+'use strict';
+
+var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+
+var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
+
+var WIZARD_COATS = ['rgb(101, 137, 164)',
+  'rgb(241, 43, 107)',
+  'rgb(56, 159, 117)',
+  'rgb(215, 210, 55)',
+  'rgb(0, 0, 0)'];
+
+var WIZARD_EYES = ['black', 'red', 'blue', 'yellow', 'green'];
+
+
+var userDialog = document.querySelector('.setup');
+userDialog.classList.remove('hidden');
+
+var getRandom = function (list) {
+  return Math.floor(Math.random() * list.length);
+};
+
+var getWizard = function () {
+  var wizard = {
+    name: WIZARD_NAMES[getRandom(WIZARD_NAMES)] + ' ' + WIZARD_SURNAMES[getRandom(WIZARD_SURNAMES)],
+    coatColor: WIZARD_COATS[getRandom(WIZARD_COATS)],
+    eyesColor: WIZARD_EYES[getRandom(WIZARD_EYES)]
+  };
+  return wizard;
+};
+
+var wizards = [];
+wizards.length = 4;
+
+for (var i = 0; i < wizards.length; i++) {
+  wizards[i] = getWizard();
+}
+
+var similarListElement = userDialog.querySelector('.setup-similar-list');
+
+var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+  .content
+  .querySelector('.setup-similar-item');
+
+var renderWizard = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+}
+
+var fragment = document.createDocumentFragment();
+for (var j = 0; j < wizards.length; j++) {
+  fragment.appendChild(renderWizard(wizards[j]));
+}
+similarListElement.appendChild(fragment);
+
+userDialog.querySelector('.setup-similar').classList.remove('hidden');
