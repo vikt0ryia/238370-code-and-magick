43a68f1f964b142284ef2caaaaa0f9ff9c31069a
Vika <elegant_17@list.ru>
2018-06-07 19:55:56 +0300

    add semicolon

--- js/setup.js
+++ js/setup.js
@@ -50,7 +50,7 @@ var renderWizard = function (wizard) {
   wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
   return wizardElement;
-}
+};
 
 var fragment = document.createDocumentFragment();
 for (var j = 0; j < wizards.length; j++) {
